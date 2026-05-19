ecad6ffdee2cbefb2eb341b77be1be6fc0834561
Gabriel Teixeira <gabrieldamasqueiroz@gmail.com>
2026-05-19 19:05:09 -0300

    adicionando paginação e iniciando criação de usuário

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def index(request):
     contacts = Contact.objects\
         .filter(show=True)\
-        .order_by('-id')[0:10]
+        .order_by('-id')
 
-    
+    paginator = Paginator(contacts, 10)  
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contacts | '
     }
 
@@ -26,7 +30,7 @@ def search(request):
 
     if search_value == '':
         return redirect('contact:index')
-
+    
     contacts = Contact.objects \
         .filter(show=True)\
         .filter(
@@ -36,9 +40,14 @@ def search(request):
             Q(email__icontains=search_value)
         )\
         .order_by('-id')
+
+    paginator = Paginator(contacts, 10)  
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
         
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contacts | '
     }
 
